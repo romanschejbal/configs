0ea844bca61c5849be029407453d8fdd24f98d61
Roman Schejbal <roman.schejbal@ataccama.com>
2021-06-14 14:00:37 +0200

    Update docker-compose -> docker compose

--- config.fish
+++ config.fish
@@ -8,7 +8,7 @@ source $HOME/.cargo/env
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
 # aliases
-alias dc="docker-compose"
-alias dup="docker-compose up"
+alias dc="docker compose"
+alias dup="docker compose up"
 alias ll="exa -l"
 alias ls="exa"
